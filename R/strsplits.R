030d888170523ff1dd5471cac8908ff878bc64eb
ich-bin-tyrone <tcmariano@up.edu.ph>
2024-08-04 15:10:28 +0800

    added a new function new_func

--- R/strsplits.R
+++ R/strsplits.R
@@ -13,3 +13,17 @@
 strsplit1 <- function(x){
   x+2
 }
+
+#' Title
+#'
+#' @param x integer 1
+#' @param y integer 2
+#'
+#' @return a sum of a number and cube of a number
+#' @export
+#'
+#' @examples
+#' new_func(1,2)
+new_func <- function(x, y){
+  y+x^3
+}
